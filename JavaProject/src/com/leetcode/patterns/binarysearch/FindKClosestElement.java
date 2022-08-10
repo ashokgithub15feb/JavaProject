1856b9123d027f052404259e2c56910b204ae79f
ashokgithub15feb <ashokkumar10792@gmail.com>
2022-08-11 00:39:58 +0530

    Closest element

--- JavaProject/src/com/leetcode/patterns/binarysearch/FindKClosestElement.java
+++ JavaProject/src/com/leetcode/patterns/binarysearch/FindKClosestElement.java
@@ -1,6 +1,7 @@
 package com.leetcode.patterns.binarysearch;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -42,9 +43,96 @@ public class FindKClosestElement {
 		int x = 45;
 		int k = 3;
 		
+//		int[] arr = {1, 5, 10};
+//		int x = 4;
+//		int k = 1;
+		
+//		int[] arr = {1, 2, 3,4,5};
+//		int x = -1;
+//		int k = 4;
+		
+//		int[] arr = {1,2,3,4,5};
+//		int x = 4;
+//		int k = 3;
+		
+		System.out.println("Input: "+Arrays.toString(arr)+", X: "+x+", K: "+k);
+		
 		List<Integer> result = findKClosestElement(arr, x, k);
 		
 		System.out.println("FindKClosestElement: "+result);
+		
+		System.out.println("===================================");
+
+		List<Integer> result1 = findKClosestElement1(arr, x, k);
+		
+		System.out.println("FindKClosestElement1: "+result1);
+		
+		System.out.println("==================================");
+		
+		System.out.println("FindKClosestElement1: "+findClosestElements(arr, k, x));
+		
+	}
+
+	private static List<Integer> findKClosestElement1(int[] arr, int x, int k) {
+		
+		int low = 0;
+		int high = arr.length-1;
+		int mid = 0;
+		
+		while(low <= high)
+		{
+			mid = low + (high - low)/2;
+			
+			if(arr[mid] == x)
+			{
+				break;
+			}
+			else if(arr[mid] < x)
+			{
+				low = mid + 1;
+			}
+			else
+			{
+				high = mid - 1;
+			}
+		}
+		
+		int left = Math.max(0, mid - 1);
+		int right = left + 1;
+		List<Integer> list = new ArrayList<>();
+		
+		
+		while(left >=0 && right < arr.length && k > 0)
+		{
+			if(Math.abs(arr[left] - x) <= Math.abs(arr[right] - x))
+			{
+				list.add(arr[left]);
+				left--;
+			}
+			else
+			{
+				list.add(arr[right]);
+				right++;
+			}
+			k--;
+		}
+		
+		while(k > 0 && left >= 0)
+		{
+			list.add(arr[left]);
+			left--;
+			k--;
+		}
+		
+		while(k > 0 && right < arr.length)
+		{
+			list.add(arr[right]);
+			right++;
+			k--;
+		}
+		
+		Collections.sort(list);
+		return list;
 	}
 
 	private static List<Integer> findKClosestElement(int[] arr, int x, int k) {
@@ -79,4 +167,26 @@ public class FindKClosestElement {
 		
 		return list;
 	}
+	
+	 public static List<Integer> findClosestElements(int[] arr, int k, int x) {
+	        int left = 0;
+	        int right = arr.length - k;
+
+	        while (left < right) {
+	            int mid = (left + right) / 2;
+	            if (x - arr[mid] > arr[mid + k] - x) {
+	                left = mid + 1;
+	            } else {
+	                right = mid;
+	            }
+	        }
+
+	        List<Integer> result = new ArrayList<Integer>();
+	        for (int i = left; i < left + k; i++) {
+	            result.add(arr[i]);
+	        }
+
+	        return result;
+	    }
+	 
 }
