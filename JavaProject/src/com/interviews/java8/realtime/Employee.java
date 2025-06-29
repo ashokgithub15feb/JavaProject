81f574007b5be0e318ef388f2369ec95d1509fb3
ashokgithub15feb <ashokkumar10792@gmail.com>
2025-06-29 21:44:13 +0530

    java 8 real time coding practice

---
+++ JavaProject/src/com/interviews/java8/realtime/Employee.java
@@ -0,0 +1,131 @@
+package com.interviews.java8.realtime;
+
+public class Employee {
+
+	private int id;
+	
+	private String name;
+	
+	private int age;
+	
+	private String gender;
+	
+	private String department;
+	
+	private int yearOfJoining;
+	
+	private double salary;
+
+	public Employee() {
+	}
+	
+	public Employee(int id, String name, int age, String gender, String department, int yearOfJoining, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.department = department;
+		this.yearOfJoining = yearOfJoining;
+		this.salary = salary;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	/**
+	 * @return the department
+	 */
+	public String getDepartment() {
+		return department;
+	}
+
+	/**
+	 * @param department the department to set
+	 */
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	/**
+	 * @return the yearOfJoining
+	 */
+	public int getYearOfJoining() {
+		return yearOfJoining;
+	}
+
+	/**
+	 * @param yearOfJoining the yearOfJoining to set
+	 */
+	public void setYearOfJoining(int yearOfJoining) {
+		this.yearOfJoining = yearOfJoining;
+	}
+
+	/**
+	 * @return the salary
+	 */
+	public double getSalary() {
+		return salary;
+	}
+
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+}
